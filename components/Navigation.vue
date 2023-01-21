08878b58d7cc2bdd5fbbfefe87f93d56dd6f1ea9
seriouslysean <sean@codeserious.com>
2023-01-20 19:57:12 -0500

    add explanation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,12 +2,18 @@
   <div class="navigation-container">
     <div class="links">
       <h2>NuxtLink</h2>
+      <p>These links will not work with a default click handler</p>
       <NuxtLink to="/" @click="handleClick">Home</NuxtLink>
       <NuxtLink to="/about" @click="handleClick">About</NuxtLink>
     </div>
 
     <div class="links">
       <h2>NuxtLink (via BaseLink</h2>
+      <p>
+        These links will will work with a default click handler by applying the
+        click events to an inner element and letting event bubbling handle the
+        priority (span first -> route operation).
+      </p>
       <BaseLink to="/" @click="handleClick">Home</BaseLink>
       <BaseLink to="/about" @click="handleClick">About</BaseLink>
     </div>
